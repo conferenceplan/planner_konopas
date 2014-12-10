cc2ea7f670f462d2f9068986c3548a2186ae9196
Henry Balen <henry@grenadinetech.com>
2014-12-10 18:13:35 -0500

    refactor controller additions

--- lib/decorators/lib/planner/controller_additions_decorator.rb
+++ lib/decorators/lib/planner/controller_additions_decorator.rb
@@ -1,8 +1,8 @@
 #
-# Mobile
 #
-ActionController::Base.class_eval do
-  
+#
+Planner::ControllerAdditions.class_eval do
+
     def top_menu_additions_with_konopas
       return top_menu_additions_without_konopas.merge!({ 
         "konopas"   =>
@@ -13,5 +13,5 @@ ActionController::Base.class_eval do
     end
 
     alias_method_chain :top_menu_additions, :konopas
-
+  
 end
