743e297dcbe11b813c1c990bd2af4d924db2e8b7
Henry Balen <henry@grenadinetech.com>
2014-12-15 15:04:16 -0500

    add in menu display control

--- lib/decorators/lib/planner/controller_additions_decorator.rb
+++ lib/decorators/lib/planner/controller_additions_decorator.rb
@@ -5,10 +5,10 @@ Planner::ControllerAdditions.class_eval do
 
     def top_menu_additions_with_konopas
       return top_menu_additions_without_konopas.merge!({ 
-        "konopas"   =>
+        "konopas"   => allowed?(:konopas) ?
               { :target => '/konopas/pages/konopas_dash/config', 
                 :icon => "glyphicon glyphicon-globe",
-                :target_base => '/konopas/pages/konopas_dash'}
+                :target_base => '/konopas/pages/konopas_dash'} : nil
        }.keep_if{|k,v| v})
     end
 
