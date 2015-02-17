7a0265c7f3fd1248ca0c60cf429a1aa259ee90f3
Henry Balen <henry@grenadinetech.com>
2015-02-17 10:08:10 -0500

    refactor for menu

---
+++ lib/decorators/cells/configs_cell_decorator.rb
@@ -0,0 +1,27 @@
+#
+#
+#
+Admin::ConfigsCell.class_eval do
+
+  def templates_with_konopas
+    @extra_template = [] if !@extra_template
+    @extra_template << "konopas_config_template"
+    templates_without_konopas
+  end
+  alias_method_chain :templates, :konopas
+
+  def javascript_with_konopas
+    @extra_javascript = [] if ! @extra_javascript
+    @extra_javascript << "konopas_config_javascript"
+    javascript_without_konopas
+  end
+  alias_method_chain :javascript, :konopas
+
+  def display_with_konopas
+    @extra_config_menu = [] if ! @extra_config_menu
+    @extra_config_menu << 'konopas_config_menu'
+    display_without_konopas
+  end
+  alias_method_chain :display, :konopas
+
+end
