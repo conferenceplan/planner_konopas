16bfb91c4e8e84d2012c80c61498cde8ffb36cdf
Henry Balen <henry@grenadinetech.com>
2015-07-21 20:47:46 -0400

    small changes

--- app/controllers/planner_konopas/program_controller.rb
+++ app/controllers/planner_konopas/program_controller.rb
@@ -13,8 +13,6 @@ module PlannerKonopas
       stream = params[:stream]
       layout = params[:layout]
       day = params[:day]
-      name = params[:name]
-      lastname = params[:lastname]
       @singleVenue = Venue.count == 1
       
       PublishedProgrammeItem.uncached do
@@ -23,7 +21,7 @@ module PlannerKonopas
         if stream
           @programmeItems = PublishedProgramItemsService.getTaggedPublishedProgramItems stream, day, name, lastname
         else
-          @programmeItems = PublishedProgramItemsService.getPublishedProgramItems day, name, lastname
+          @programmeItems = PublishedProgramItemsService.getPublishedProgramItems day
         end
       end
       
