c5c35f3c664cbc72be5ad5236a89cc0dbb7fa328
balen <henry@grenadinetech.com>
2017-01-09 20:08:03 -0500

    initial conversion for Rails 4.2

--- lib/planner_konopas/engine.rb
+++ lib/planner_konopas/engine.rb
@@ -13,12 +13,12 @@ module PlannerKonopas
 
     # RAILS 3 mechanism so parent app use the migrations in this engine
     # see http://pivotallabs.com/leave-your-migrations-in-your-rails-engines/
-    initializer :append_migrations do |app|
-      # unless the engine is the multi-tenant planner_front
-      unless (app.root.to_s.match root.to_s) || (app.engine_name == "planner_front_application")
-        app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
-      end
-    end
+    # initializer :append_migrations do |app|
+      # # unless the engine is the multi-tenant planner_front
+      # unless (app.root.to_s.match root.to_s) || (app.engine_name == "planner_front_application")
+        # app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
+      # end
+    # end
 
   end
 end
