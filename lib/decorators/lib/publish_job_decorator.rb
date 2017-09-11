a30c8cd05d45a8309b9c193be78c9be8a3f6bae2
balen <henry@grenadinetech.com>
2017-09-11 10:58:23 -0400

    fixes

--- lib/decorators/lib/publish_job_decorator.rb
+++ lib/decorators/lib/publish_job_decorator.rb
@@ -2,34 +2,36 @@ require "net/http"
 
 PublishJob.class_eval do
 
-  # def post_process_with_konopas
-  #   # do the original job
-  #   post_process_without_konopas
-  # 
-  #   begin
-  #     # get the Konopas config
-  #     config = PlannerKonopas::KonopasConfig.first
-  #     
-  #     if config
-  #       # Prime the cache
-  #       if !config.base_url.blank?
-  #         make_request(config.base_url + '/konopas/program.js')
-  #         make_request(config.base_url + '/konopas/program/participants.js')
-  #       end
-  #       
-  #       # and now reset the Konopas manifest
-  #       make_request(config.manifest_url,  config.manifest_user,  config.manifest_password) if !config.manifest_url.blank?
-  #       make_request(config.calendar_url, config.calendar_user, config.calendar_password) if !config.calendar_url.blank?
-  #       # uri = URI.parse('https://konopas-server.appspot.com/loncon3/update_prog') <div style="padding:10px; padding-top: 0px; padding-bottom: 0px;">
-  #       # uri = URI.parse('http://guide.loncon3.org/util/update-cache-manifest.php')
-  #     end
-  #   rescue => ex
-  #     # so we do not force a repulish if there is an issue
-  #     # TODO - log to say what happened
-  #   end
-  # end
-  # 
-  # alias_method_chain :post_process, :konopas
+  def post_process_with_konopas
+    # do the original job
+    post_process_without_konopas
+  
+    begin
+      # get the Konopas config
+      if FeatureService.has? 'konopas'
+        config = PlannerKonopas::KonopasConfig.first
+        
+        if config
+          # Prime the cache
+          if !config.base_url.blank?
+            make_request(config.base_url + '/konopas/program.js')
+            make_request(config.base_url + '/konopas/program/participants.js')
+          end
+          
+          # and now reset the Konopas manifest
+          make_request(config.manifest_url,  config.manifest_user,  config.manifest_password) if !config.manifest_url.blank?
+          make_request(config.calendar_url, config.calendar_user, config.calendar_password) if !config.calendar_url.blank?
+          # uri = URI.parse('https://konopas-server.appspot.com/loncon3/update_prog') <div style="padding:10px; padding-top: 0px; padding-bottom: 0px;">
+          # uri = URI.parse('http://guide.loncon3.org/util/update-cache-manifest.php')
+        end
+      end
+    rescue => ex
+      # so we do not force a repulish if there is an issue
+      # TODO - log to say what happened
+    end
+  end
+  
+  alias_method_chain :post_process, :konopas
 
 #  protected
 
--- lib/planner_konopas/version.rb
+++ lib/planner_konopas/version.rb
@@ -1,3 +1,3 @@
 module PlannerKonopas
-  VERSION = "1.1.56"
+  VERSION = "1.1.57"
 end
